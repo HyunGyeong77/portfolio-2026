d24de55badb6eda2250d52e8ef0982a21fe58a8a
Jwiroo <qorudgus77@gmail.com>
2026-03-05 17:39:53 +0900

    style(loading): add client component style

--- app/components/ClientComponent.tsx
+++ app/components/ClientComponent.tsx
@@ -1,8 +1,9 @@
+import styles from './client-component.module.scss';
 import Loading from '@common/loading/Loading';
 
 export default function ClientComponent() {
   return (
-    <div>
+    <div className={styles.wrap}>
       <Loading />
       <main></main>
     </div>
