da66df2d59f11afbacba4667afef300560daf196
Jwiroo <qorudgus77@gmail.com>
2026-03-05 18:21:53 +0900

    fix(loading): remove shape-rendering attribute to LoadingSvg

--- components/common/loading/components/LoadingSvg.tsx
+++ components/common/loading/components/LoadingSvg.tsx
@@ -5,8 +5,7 @@ export default function LoadingSvg() {
       width="100%"
       height="100%"
       viewBox="0 0 32 6"
-      xmlns="http://www.w3.org/2000/svg" 
-      shape-rendering="crispEdges"
+      xmlns="http://www.w3.org/2000/svg"
     >
       <rect x="1" y="0" width="30" height="1" fill="#FFFFFF" />
       <rect x="0" y="1" width="1" height="4" fill="#FFFFFF" />
