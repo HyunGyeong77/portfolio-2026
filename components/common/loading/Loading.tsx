27141129626f90a51ec5a7c6ed13b5bfaf65ed8f
Jwiroo <qorudgus77@gmail.com>
2026-03-05 20:50:25 +0900

    add Loading component animation and loading.svg

--- components/common/loading/Loading.tsx
+++ components/common/loading/Loading.tsx
@@ -2,19 +2,42 @@
 
 import styles from './loading.module.scss';
 import LoadingGif from './assets/loading.gif';
+import LoadingSvg from './assets/loading.svg';
 import ProgressBar from './components/ProgressBar';
 import Image from 'next/image';
 import {useState, useEffect, useRef} from 'react';
+import gsap from 'gsap';
 
 export default function Loading() {
-  const [percent, setPercent] = useState(0);
-  const percentRef = useRef(0);
+  // 퍼센트
+  const [percent, setPercent] = useState<number>(0);
+  const percentRef = useRef<number>(0);
+
+  // document가 준비된 상태인지 확인
+  const [isReady, setIsReady] = useState(false);
+
+  // document가 준비가 됐고 애니메이션이 끝났는지 확인
+  const [isLoading, setIsLoading] = useState(true);
+
+  // DOM Ref
+  const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const isComplete = () => {
       if(document.readyState === "complete") {
         setPercent(100);
+        setIsReady(true);
         clearInterval(interval);
+
+        if(!containerRef.current) return;
+        gsap.to(containerRef.current, {
+          opacity: 0, 
+          duration: 0.5,
+          delay: 0.5,
+          onComplete: () => {
+            setIsLoading(false);
+          }
+        });
       }
     }
 
@@ -37,10 +60,19 @@ export default function Loading() {
     return () => clearInterval(interval);
   }, []);
 
+  if(!isLoading) return null;
+
   return (
-    <div className={styles.loading}>
+    <div ref={containerRef} className={styles.loading}>
       <div className={styles.loading__inner}>
-          <Image className={styles.loading__gif} src={LoadingGif} alt="loading gif" width={37} height={6} loading="eager" />
+          <Image 
+            className={styles.loading__gif} 
+            src={!isReady ? LoadingGif : LoadingSvg} 
+            alt="loading image" 
+            width={37} 
+            height={6} 
+            loading="eager" 
+          />
           <div className={styles.loading__progressbar}>
             <ProgressBar />
             <div className={styles["loading__progress-box"]}>
