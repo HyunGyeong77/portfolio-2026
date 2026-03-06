eb0576050d01715afd350438154757d7c37729e7
Jwiroo <qorudgus77@gmail.com>
2026-03-06 12:00:56 +0900

    chore(header): initialize header component

--- app/components/ClientComponent.tsx
+++ app/components/ClientComponent.tsx
@@ -1,10 +1,12 @@
 import styles from './client-component.module.scss';
 import Loading from '@common/loading/Loading';
+import Header from '@layout/header/Header';
 
 export default function ClientComponent() {
   return (
     <div className={styles.wrap}>
       <Loading />
+      <Header />
       <main></main>
     </div>
   );
