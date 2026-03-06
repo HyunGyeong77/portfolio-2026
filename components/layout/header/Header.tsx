10c57160de24f66b0a1fe43cb2d597e81607feef
Jwiroo <qorudgus77@gmail.com>
2026-03-06 13:03:27 +0900

    feat(header): add mobile media query and render

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -1,9 +1,14 @@
+"use client";
 
+import {useMediaQuery} from '@/lib/hooks/useMediaQuery';
+import HeaderMobile from './mobile/HeaderMobile';
 
 export default function Header() {
-  return (
-    <header>
-      
-    </header>
-  );
+  const {width, isMobile} = useMediaQuery();
+
+  if(width === null) return null;
+
+  if(isMobile) {
+    return <HeaderMobile />
+  }
 }
