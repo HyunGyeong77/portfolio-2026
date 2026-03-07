09d3c0b35f50d3efdce5f23dda67667947541edd
Jwiroo <qorudgus77@gmail.com>
2026-03-07 19:21:30 +0900

    feat(main-menu): render background field

--- app/components/home/main-menu/MainMenu.tsx
+++ app/components/home/main-menu/MainMenu.tsx
@@ -1,9 +1,13 @@
 import styles from './main-menu.module.scss';
+import Image from 'next/image';
+import Background from './assets/img/background.png';
 
 export default function MainMenu() {
   return (
     <div className={styles.main}>
-      
+      <div className={styles["main__bg-box"]}>
+        <Image className={styles.main__bg} src={Background} alt="background image" width={1400} height={700} />
+      </div>
     </div>
   );
 }
