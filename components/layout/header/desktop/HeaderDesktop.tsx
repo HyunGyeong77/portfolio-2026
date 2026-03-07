db696c44794ccce5d1a87fdfd9305e90b2aa3b6f
Jwiroo <qorudgus77@gmail.com>
2026-03-07 15:51:32 +0900

    feat(header): add Repo and Follower component to HeaderDesktop component

--- components/layout/header/desktop/HeaderDesktop.tsx
+++ components/layout/header/desktop/HeaderDesktop.tsx
@@ -1,14 +1,31 @@
+import styles from './header-desktop.module.scss';
 import Stats from '@layout/header/components/stats/Stats';
+import Repo from '@layout/header/components/resource/repo/Repo';
+import Follower from '@layout/header/components/resource/follower/Follower';
 
 export default function HeaderDesktop() {
   return (
-    <header>
+    <header className={styles.header}>
       <Stats 
         statsWidth="400px"
         statsHeight="204px"
         expWidth={201}
         expHeight={21}
       />
+      <div className={styles.header__info}>
+        <Repo
+          bgWidth={250}
+          bgHeight={57}
+          linkWidth={35}
+          linkHeight={39}
+        />
+        <Follower 
+          bgWidth={250}
+          bgHeight={57}
+          linkWidth={35}
+          linkHeight={39}
+        />
+      </div>
     </header>
   );
 }
