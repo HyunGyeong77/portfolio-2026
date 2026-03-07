ca972227e6e9ae62e12cd97e730482ca51701848
Jwiroo <qorudgus77@gmail.com>
2026-03-07 19:02:28 +0900

    feat(main-menu): initialize MainMenu component

--- app/components/ClientComponent.tsx
+++ app/components/ClientComponent.tsx
@@ -6,6 +6,7 @@ import Header from '@layout/header/Header';
 import {PageContext} from '@layout/header/components/stats/Stats';
 import {useState} from 'react';
 import TargetCursor from '@common/cursor/TargetCursor';
+import MainMenu from './home/main-menu/MainMenu';
 
 export default function ClientComponent() {
   const [page, setPage] = useState<string>("hero");
@@ -19,6 +20,7 @@ export default function ClientComponent() {
         parallaxOn
         hoverDuration={0.2}
       />
+      <MainMenu />
       <PageContext.Provider value={page}>
         <Header />
       </PageContext.Provider>
---
+++ app/components/home/main-menu/MainMenu.tsx
@@ -0,0 +1,9 @@
+import styles from './main-menu.module.scss';
+
+export default function MainMenu() {
+  return (
+    <div className={styles.main}>
+      
+    </div>
+  );
+}
