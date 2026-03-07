39e72c1ce951933e9c0448da4235d6fa8259b28e
Jwiroo <qorudgus77@gmail.com>
2026-03-07 15:13:18 +0900

    feat(header): render Follower and Repo in Menu component

--- components/layout/header/mobile/components/menu/Menu.tsx
+++ components/layout/header/mobile/components/menu/Menu.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import styles from './menu.module.scss';
-import Stats from '@layout/header/components/stats/Stats'
+import Stats from '@layout/header/components/stats/Stats';
+import Repo from '@layout/header/components/resource/repo/Repo';
+import Follower from '@layout/header/components/resource/follower/Follower';
 
 export default function Menu() {
+
   return (
     <div className={styles.menu}>
       <Stats 
@@ -10,6 +15,10 @@ export default function Menu() {
         expWidth={157}
         expHeight={19}
       />
+      <div className={styles.menu__info}>
+        <Repo bgWidth={311} bgHeight={70} linkWidth={42} linkHeight={47} />
+        <Follower bgWidth={311} bgHeight={70} linkWidth={42} linkHeight={47} />
+      </div>
     </div>
   );
 }
