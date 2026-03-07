e288fe78d594650e10050b92217d5554443a5978
Jwiroo <qorudgus77@gmail.com>
2026-03-07 15:11:39 +0900

    chore(header): add Resource component

---
+++ components/layout/header/components/resource/Resource.tsx
@@ -0,0 +1,36 @@
+import styles from './resource.module.scss';
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import Image from 'next/image';
+import Link from 'next/link';
+
+type ImageType = {
+  src: StaticImport
+  alt: string
+  width: number
+  height: number
+}
+
+type Props = {
+  bg: ImageType
+  link: ImageType
+  href: string
+  title: string
+  resource: string | undefined
+  isLoading: boolean
+}
+
+export default function Resource(props: Props) { 
+  const {bg, link, href, title, resource, isLoading} = props;
+
+  return (
+    <div className={styles.resource}>
+      <Image src={bg.src} alt={bg.alt} width={bg.width} height={bg.height} />
+      <div className={styles.resource__box}>
+        <p>{isLoading ? "로딩중.." : resource}</p>
+        <Link className={styles.resource__link} href={href} title={title} target="_blank">
+          <Image src={link.src} alt={link.alt} width={link.width} height={link.height} />
+        </Link>
+      </div>
+    </div>
+  );
+}
