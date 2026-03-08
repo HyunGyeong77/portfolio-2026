8d734c475ed1b7f68b983d8c08dfe7070df938a1
Jwiroo <qorudgus77@gmail.com>
2026-03-08 10:28:53 +0900

    fix(target-cursor): remount TargetCursor by controlling key with state

--- app/components/ClientComponent.tsx
+++ app/components/ClientComponent.tsx
@@ -4,25 +4,35 @@ import styles from './client-component.module.scss';
 import Loading from '@common/loading/Loading';
 import Header from '@layout/header/Header';
 import {PageContext} from '@layout/header/components/stats/Stats';
-import {useState} from 'react';
+import {useState, createContext} from 'react';
 import TargetCursor from '@common/cursor/TargetCursor';
+
+export const CursorContext = createContext<(() => void) | null>(null);
 
 export default function ClientComponent() {
   const [page, setPage] = useState<string>("hero");
+  const [cursorKey, setCursorKey] = useState<number>(0);
+
+  const cursorRemount = () => {
+    setCursorKey(prev => prev + 1);
+  }
 
   return (
     <div className={styles.wrap}>
       <Loading />
       <TargetCursor 
+        key={cursorKey}
         spinDuration={2}
         hideDefaultCursor
         parallaxOn
         hoverDuration={0.2}
       />
-      <PageContext.Provider value={page}>
-        <Header />
-      </PageContext.Provider>
-      <main></main>
+      <CursorContext value={cursorRemount}>
+        <PageContext.Provider value={page}>
+          <Header />
+        </PageContext.Provider>
+        <main></main>
+      </CursorContext>
     </div>
   );
 }
