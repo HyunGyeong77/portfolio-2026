6741f06197817be6997629a84370f97f8ad2ba07
Jwiroo <qorudgus77@gmail.com>
2026-03-08 11:48:08 +0900

    chore(main-menu): add CloseSvg component

---
+++ app/components/home/main-menu/components/menu/components/CloseSvg.tsx
@@ -0,0 +1,64 @@
+
+
+export default function CloseSvg() {
+  return (
+    <svg 
+      version="1.1" 
+      width="100%" 
+      height="100%"
+      viewBox="0 0 16 16"
+      xmlns="http://www.w3.org/2000/svg" 
+      shapeRendering="crispEdges"
+    >
+      <rect x="2" y="0" width="12" height="1" fill="#FFFFFF"/>
+      <rect x="1" y="1" width="2" height="1" fill="#FFFFFF"/>
+      <rect x="3" y="1" width="10" height="1" fill="#F1DFFF"/>
+      <rect x="13" y="1" width="2" height="1" fill="#FFFFFF"/>
+      <rect x="0" y="2" width="2" height="1" fill="#FFFFFF"/>
+      <rect x="2" y="2" width="1" height="1" fill="#F1DFFF"/>
+      <rect x="3" y="2" width="10" height="2" fill="#DFB5FF"/>
+      <rect x="13" y="2" width="1" height="1" fill="#F1DFFF"/>
+      <rect x="14" y="2" width="2" height="1" fill="#FFFFFF"/>
+      <rect x="0" y="3" width="1" height="11" fill="#FFFFFF"/>
+      <rect x="1" y="3" width="1" height="1" fill="#F1DFFF"/>
+      <rect x="2" y="3" width="1" height="10" fill="#DFB5FF"/>
+      <rect x="13" y="3" width="1" height="10" fill="#DFB5FF"/>
+      <rect x="14" y="3" width="1" height="1" fill="#F1DFFF"/>
+      <rect x="15" y="3" width="1" height="11" fill="#FFFFFF"/>
+      <rect x="1" y="4" width="1" height="9" fill="#D09BF9"/>
+      <rect x="3" y="4" width="1" height="10" fill="#DFB5FF"/>
+      <rect x="4" y="4" width="2" height="2" fill="#FFFFFF"/>
+      <rect x="6" y="4" width="4" height="1" fill="#DFB5FF"/>
+      <rect x="10" y="4" width="2" height="2" fill="#FFFFFF"/>
+      <rect x="12" y="4" width="1" height="10" fill="#DFB5FF"/>
+      <rect x="14" y="4" width="1" height="9" fill="#D09BF9"/>
+      <rect x="6" y="5" width="1" height="6" fill="#FFFFFF"/>
+      <rect x="7" y="5" width="2" height="1" fill="#DFB5FF"/>
+      <rect x="9" y="5" width="1" height="6" fill="#FFFFFF"/>
+      <rect x="4" y="6" width="1" height="4" fill="#DFB5FF"/>
+      <rect x="5" y="6" width="1" height="1" fill="#FFFFFF"/>
+      <rect x="7" y="6" width="2" height="4" fill="#FFFFFF"/>
+      <rect x="10" y="6" width="1" height="1" fill="#FFFFFF"/>
+      <rect x="11" y="6" width="1" height="4" fill="#DFB5FF"/>
+      <rect x="5" y="7" width="1" height="2" fill="#DFB5FF"/>
+      <rect x="10" y="7" width="1" height="2" fill="#DFB5FF"/>
+      <rect x="5" y="9" width="1" height="3" fill="#FFFFFF"/>
+      <rect x="10" y="9" width="1" height="3" fill="#FFFFFF"/>
+      <rect x="4" y="10" width="1" height="2" fill="#FFFFFF"/>
+      <rect x="7" y="10" width="2" height="4" fill="#DFB5FF"/>
+      <rect x="11" y="10" width="1" height="2" fill="#FFFFFF"/>
+      <rect x="6" y="11" width="1" height="3" fill="#DFB5FF"/>
+      <rect x="9" y="11" width="1" height="3" fill="#DFB5FF"/>
+      <rect x="4" y="12" width="2" height="2" fill="#DFB5FF"/>
+      <rect x="10" y="12" width="2" height="2" fill="#DFB5FF"/>
+      <rect x="1" y="13" width="1" height="2" fill="#FFFFFF"/>
+      <rect x="2" y="13" width="1" height="1" fill="#D09BF9"/>
+      <rect x="13" y="13" width="1" height="1" fill="#D09BF9"/>
+      <rect x="14" y="13" width="1" height="2" fill="#FFFFFF"/>
+      <rect x="2" y="14" width="1" height="2" fill="#FFFFFF"/>
+      <rect x="3" y="14" width="10" height="1" fill="#D09BF9"/>
+      <rect x="13" y="14" width="1" height="2" fill="#FFFFFF"/>
+      <rect x="3" y="15" width="10" height="1" fill="#FFFFFF"/>
+    </svg>
+  );
+}
