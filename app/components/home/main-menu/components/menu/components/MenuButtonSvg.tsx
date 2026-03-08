bb66c6256f4969027cb048c5ddef200d7e7098e5
Jwiroo <qorudgus77@gmail.com>
2026-03-08 11:48:49 +0900

    chore(main-menu): add MenuButtonSvg component

---
+++ app/components/home/main-menu/components/menu/components/MenuButtonSvg.tsx
@@ -0,0 +1,32 @@
+
+
+export default function MenuButtonSvg() {
+  return (
+    <svg 
+      version="1.1" 
+      width="100%" 
+      height="100%" 
+      viewBox="0 0 48 16"
+      xmlns="http://www.w3.org/2000/svg" 
+      shapeRendering="crispEdges"
+    >
+      <rect x="2" y="0" width="44" height="1" fill="#4F4F4F"/>
+      <rect x="1" y="1" width="1" height="1" fill="#4F4F4F"/>
+      <rect x="2" y="1" width="44" height="11" fill="#FFFFFF"/>
+      <rect x="46" y="1" width="1" height="1" fill="#4F4F4F"/>
+      <rect x="0" y="2" width="1" height="12" fill="#4F4F4F"/>
+      <rect x="1" y="2" width="1" height="9" fill="#FFFFFF"/>
+      <rect x="46" y="2" width="1" height="9" fill="#FFFFFF"/>
+      <rect x="47" y="2" width="1" height="12" fill="#4F4F4F"/>
+      <rect x="1" y="11" width="1" height="3" fill="#D7D7D7"/>
+      <rect x="46" y="11" width="1" height="3" fill="#D7D7D7"/>
+      <rect x="2" y="12" width="1" height="3" fill="#D7D7D7"/>
+      <rect x="3" y="12" width="42" height="1" fill="#FFFFFF"/>
+      <rect x="45" y="12" width="1" height="3" fill="#D7D7D7"/>
+      <rect x="3" y="13" width="42" height="2" fill="#D7D7D7"/>
+      <rect x="1" y="14" width="1" height="1" fill="#4F4F4F"/>
+      <rect x="46" y="14" width="1" height="1" fill="#4F4F4F"/>
+      <rect x="2" y="15" width="44" height="1" fill="#4F4F4F"/>
+    </svg>
+  );
+}
