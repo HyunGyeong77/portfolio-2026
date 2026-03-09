8727989873b51a12e4a38cfaf05b2a9be3ad7e0d
Jwiroo <qorudgus77@gmail.com>
2026-03-09 21:43:59 +0900

    feat(footer): render FooterDesktop in Footer component

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -1,12 +1,15 @@
 import {useMediaQuery} from "@/lib/hooks/useMediaQuery";
 import FooterMobile from "./mobile/FooterMobile";
+import FooterDesktop from "./desktop/FooterDesktop";
 
 export default function Footer() {
-  const {width, isMobile} = useMediaQuery();
+  const {width, isMobile, isTablet} = useMediaQuery();
 
   if(width === null) return null;
 
-  if(isMobile) {
+  if(isMobile || isTablet) {
     return <FooterMobile />
+  } else {
+    return <FooterDesktop />
   }
 }
