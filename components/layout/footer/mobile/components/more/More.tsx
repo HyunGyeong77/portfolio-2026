200ef3044dd431913f8f328e0a194196085683b3
Jwiroo <qorudgus77@gmail.com>
2026-03-09 20:29:00 +0900

    feat(footer): render Navigation component to More component

--- components/layout/footer/mobile/components/more/More.tsx
+++ components/layout/footer/mobile/components/more/More.tsx
@@ -1,20 +1,38 @@
 import styles from './more.module.scss';
 import MoreSvg from './components/MoreSvg';
-import {useContext} from 'react';
+import {useContext, useState, useEffect} from 'react';
 import {PageContext} from '@/components/layout/header/components/stats/Stats';
 import {pageList} from '@/lib/constants/constants';
+import Navigation from '@layout/footer/components/navigation/Navigation';
 
 export default function More() {
   const page = useContext(PageContext);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleClick = () => {
+    setIsMenuOpen(prev => !prev);
+  }
+
+  useEffect(() => {
+    if(!isMenuOpen) return;
+
+    const menuClose = () => {
+      setIsMenuOpen(false);
+    }
+
+    document.addEventListener("click", menuClose);
+    return () => document.removeEventListener("click", menuClose);
+  }, [isMenuOpen]);
 
   return (
     <div className={styles.more}>
       <button 
-        className={`${styles.more__button} ${(page !== pageList.mainMenu) && "cursor-target"}`}
+        className={`${styles.more__button} ${(page !== pageList.mainMenu) && "cursor-target"} ${isMenuOpen && styles["menu-open"]}`}
+        onClick={toggleClick}
       >
         <MoreSvg />
       </button>
-      
+      <Navigation isMenuOpen={isMenuOpen} />
     </div>
   );
 }
