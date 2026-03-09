01fc48a4a27e5ae69df8b9936f0f39c542f015e2
Jwiroo <qorudgus77@gmail.com>
2026-03-09 11:04:01 +0900

    feat(main-menu): add onclick event handler

--- app/components/home/main-menu/components/key-button/components/button/Button.tsx
+++ app/components/home/main-menu/components/key-button/components/button/Button.tsx
@@ -1,5 +1,8 @@
 import styles from './button.module.scss';
 import ButtonSvg from '../ButtonSvg';
+import {useContext} from 'react';
+import {CursorContext} from '@/app/components/ClientComponent';
+import {pageList} from '@/lib/constants/constants';
 
 type Props = {
   content: string,
@@ -8,9 +11,11 @@ type Props = {
 
 export default function Button(props: Props) {
   const {content, onClick} = props;
+  const cursorTarget = useContext(CursorContext);
+  if(!cursorTarget) return null;
 
   return (
-    <button className={`${styles.button} cursor-target`} onClick={onClick}>
+    <button className={`${styles.button} ${cursorTarget(pageList.mainMenu)}`} onClick={onClick}>
       <ButtonSvg />
       <div className={styles["button__text-box"]}>
         <p className={styles.button__text}>{content}</p>
--- app/components/home/main-menu/components/key-button/components/start/Start.tsx
+++ app/components/home/main-menu/components/key-button/components/start/Start.tsx
@@ -1,7 +1,12 @@
 import Button from '../button/Button';
+import {useContext} from 'react';
+import {PageChangeContext} from '@/app/components/ClientComponent';
+import {pageList} from '@/lib/constants/constants';
 
 export default function Start() {
+  const pageChange = useContext(PageChangeContext);
+
   return (
-    <Button content="시작하기" />
+    <Button content="시작하기" onClick={() => pageChange?.mainMenuHide(pageList.about)} />
   );
 }
