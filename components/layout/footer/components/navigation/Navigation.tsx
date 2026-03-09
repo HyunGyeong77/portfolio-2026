6687de318f0346d702056c32d5a5a8f127dd411d
Jwiroo <qorudgus77@gmail.com>
2026-03-09 20:28:06 +0900

    chore(footer): create Navigation component file

---
+++ components/layout/footer/components/navigation/Navigation.tsx
@@ -0,0 +1,23 @@
+import styles from './navigation.module.scss';
+import NavigationBtn from "@layout/footer/components/navigation/components/navigation-btn/NavigationBtn";
+import MainSvg from "@layout/footer/components/navigation/components/MainSvg";
+import AboutSvg from "@layout/footer/components/navigation/components/AboutSvg";
+import SkillsSvg from "@layout/footer/components/navigation/components/SkillsSvg";
+import ProjectsSvg from "@layout/footer/components/navigation/components/ProjectsSvg";
+
+type Props = {
+  isMenuOpen?: boolean
+}
+
+export default function Navigation(props: Props) {
+  const {isMenuOpen} = props;
+ 
+  return (
+    <ul className={`${styles.navigation} ${isMenuOpen && styles.open}`}>
+      <li><NavigationBtn icon={<MainSvg />} /></li>
+      <li><NavigationBtn icon={<AboutSvg />} /></li>
+      <li><NavigationBtn icon={<SkillsSvg />} /></li>
+      <li><NavigationBtn icon={<ProjectsSvg />} /></li>
+    </ul>
+  );
+}
