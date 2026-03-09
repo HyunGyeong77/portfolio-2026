ecb327f1ad0734761ff2022363312562fae1e87e
Jwiroo <qorudgus77@gmail.com>
2026-03-09 18:17:50 +0900

    feat(footer): change button elements to prev and next components in ArrowKey component

--- components/layout/footer/mobile/components/arrow-key/ArrowKey.tsx
+++ components/layout/footer/mobile/components/arrow-key/ArrowKey.tsx
@@ -1,11 +1,21 @@
 import styles from './arrow-key.module.scss';
-import ArrowSvg from "./components/ArrowSvg";
+import Next from './components/next/Next';
+import Prev from './components/prev/Prev';
+import {useContext} from 'react';
+import {PageContext} from '@/components/layout/header/components/stats/Stats';
+import {pageList} from '@/lib/constants/constants';
 
 export default function ArrowKey() {
+  const page = useContext(PageContext);
+
+  const isFocusPosible = () => {
+    return (page !== pageList.mainMenu) && "cursor-target";
+  }
+
   return (
     <div className={styles["arrow-key"]}>
-      <button><ArrowSvg /></button>
-      <button><ArrowSvg /></button>
+      <Prev isFocusPosible={isFocusPosible} />
+      <Next isFocusPosible={isFocusPosible} />
     </div>
   );
 }
