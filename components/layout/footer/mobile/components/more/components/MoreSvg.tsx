0cfced2d7008bc7a00a2ba06352b3a4a97283e1f
Jwiroo <qorudgus77@gmail.com>
2026-03-09 20:45:58 +0900

    style(footer): change width and height style to MoreSvg

--- components/layout/footer/mobile/components/more/components/MoreSvg.tsx
+++ components/layout/footer/mobile/components/more/components/MoreSvg.tsx
@@ -4,8 +4,8 @@ export default function MoreSvg() {
   return (
     <svg 
       version="1.1" 
-      width="56"
-      height="56"
+      width="100%"
+      height="100%"
       viewBox="0 0 16 16"
       xmlns="http://www.w3.org/2000/svg"
       shapeRendering="crispEdges"
