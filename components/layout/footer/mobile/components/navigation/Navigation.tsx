681cc3840f14965de4b5e1ee6ec57bb791ea6d70
Jwiroo <qorudgus77@gmail.com>
2026-03-09 18:20:00 +0900

    feat(footer): adjust useContext hook in more button

--- components/layout/footer/mobile/components/navigation/Navigation.tsx
+++ components/layout/footer/mobile/components/navigation/Navigation.tsx
@@ -1,13 +1,18 @@
 import styles from './navigation.module.scss';
 import More from './components/MoreSvg';
+import {useContext} from 'react';
+import {PageContext} from '@/components/layout/header/components/stats/Stats';
+import {pageList} from '@/lib/constants/constants';
 
 export default function Navigation() {
+  const page = useContext(PageContext);
+
   return (
     <div className={styles.navigation}>
       <button 
-        className={styles.navigation__more}
+        className={`${styles.navigation__more} ${(page !== pageList.mainMenu) && "cursor-target"}`}
       >
-          <More />
+        <More />
       </button>
     </div>
   );
