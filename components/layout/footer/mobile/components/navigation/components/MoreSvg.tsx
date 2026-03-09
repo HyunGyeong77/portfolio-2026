7a95b01e6f01a0b64a86c91660a4ef5f1decc3db
Jwiroo <qorudgus77@gmail.com>
2026-03-09 17:38:58 +0900

    chore(footer): create MoreSvg component file

---
+++ components/layout/footer/mobile/components/navigation/components/MoreSvg.tsx
@@ -0,0 +1,58 @@
+
+
+export default function MoreSvg() {
+  return (
+    <svg 
+      version="1.1" 
+      width="56"
+      height="56"
+      viewBox="0 0 16 16"
+      xmlns="http://www.w3.org/2000/svg"
+      shapeRendering="crispEdges"
+      >
+      <rect x="2" y="0" width="12" height="1" fill="#FFFFFF"/>
+      <rect x="1" y="1" width="2" height="1" fill="#FFFFFF"/>
+      <rect x="3" y="1" width="10" height="1" fill="#D2B5FF"/>
+      <rect x="13" y="1" width="2" height="1" fill="#FFFFFF"/>
+      <rect x="0" y="2" width="2" height="1" fill="#FFFFFF"/>
+      <rect x="2" y="2" width="1" height="1" fill="#D2B5FF"/>
+      <rect x="3" y="2" width="10" height="3" fill="#A56FF9"/>
+      <rect x="13" y="2" width="1" height="1" fill="#D2B5FF"/>
+      <rect x="14" y="2" width="2" height="1" fill="#FFFFFF"/>
+      <rect x="0" y="3" width="1" height="11" fill="#FFFFFF"/>
+      <rect x="1" y="3" width="1" height="1" fill="#D2B5FF"/>
+      <rect x="2" y="3" width="1" height="10" fill="#A56FF9"/>
+      <rect x="13" y="3" width="1" height="10" fill="#A56FF9"/>
+      <rect x="14" y="3" width="1" height="1" fill="#D2B5FF"/>
+      <rect x="15" y="3" width="1" height="11" fill="#FFFFFF"/>
+      <rect x="1" y="4" width="1" height="9" fill="#9261DE"/>
+      <rect x="14" y="4" width="1" height="9" fill="#9261DE"/>
+      <rect x="3" y="5" width="4" height="1" fill="#A56FF9"/>
+      <rect x="7" y="5" width="2" height="6" fill="#FFFFFF"/>
+      <rect x="9" y="5" width="4" height="1" fill="#A56FF9"/>
+      <rect x="3" y="6" width="3" height="1" fill="#A56FF9"/>
+      <rect x="6" y="6" width="1" height="5" fill="#FFFFFF"/>
+      <rect x="9" y="6" width="1" height="5" fill="#FFFFFF"/>
+      <rect x="10" y="6" width="3" height="1" fill="#A56FF9"/>
+      <rect x="3" y="7" width="2" height="1" fill="#A56FF9"/>
+      <rect x="5" y="7" width="1" height="4" fill="#FFFFFF"/>
+      <rect x="10" y="7" width="1" height="4" fill="#FFFFFF"/>
+      <rect x="11" y="7" width="2" height="1" fill="#A56FF9"/>
+      <rect x="3" y="8" width="1" height="1" fill="#A56FF9"/>
+      <rect x="4" y="8" width="1" height="3" fill="#FFFFFF"/>
+      <rect x="11" y="8" width="1" height="3" fill="#FFFFFF"/>
+      <rect x="12" y="8" width="1" height="1" fill="#A56FF9"/>
+      <rect x="3" y="9" width="1" height="2" fill="#FFFFFF"/>
+      <rect x="12" y="9" width="1" height="2" fill="#FFFFFF"/>
+      <rect x="3" y="11" width="10" height="3" fill="#A56FF9"/>
+      <rect x="1" y="13" width="1" height="2" fill="#FFFFFF"/>
+      <rect x="2" y="13" width="1" height="1" fill="#9261DE"/>
+      <rect x="13" y="13" width="1" height="1" fill="#9261DE"/>
+      <rect x="14" y="13" width="1" height="2" fill="#FFFFFF"/>
+      <rect x="2" y="14" width="1" height="2" fill="#FFFFFF"/>
+      <rect x="3" y="14" width="10" height="1" fill="#9261DE"/>
+      <rect x="13" y="14" width="1" height="2" fill="#FFFFFF"/>
+      <rect x="3" y="15" width="10" height="1" fill="#FFFFFF"/>
+    </svg>
+  );
+}
