33bd80281daaa62989d867787d89a1b827d57a00
Jwiroo <qorudgus77@gmail.com>
2026-03-13 12:19:42 +0900

    chore(projects): create constants file

---
+++ app/components/home/projects/components/card/constants/constants.ts
@@ -0,0 +1,20 @@
+export type Type = {
+  [key: string]: string;
+}
+
+export const skill: Type = {
+  next: "Next.js", 
+  react: "React",
+  ts: "TypeScript", 
+  js: "JavaScript",
+  postgres: "PostgreSQL", 
+  python: "Python",
+  html: "HTML", scss: "SCSS",
+}
+
+export const tag: Type = {
+  responsive: "responsive", 
+  interactive: "interactive",
+  fixed: "fixed", 
+  adaptive: "adaptive"
+}
