83ecf3efe37a47d937ae17c9d746574e497271a0
Jwiroo <qorudgus77@gmail.com>
2026-03-13 19:04:32 +0900

    feat(projects): add CurSlideContext hook

--- app/components/home/projects/components/slides/Slides.tsx
+++ app/components/home/projects/components/slides/Slides.tsx
@@ -2,19 +2,44 @@ import styles from './slides.module.scss';
 import Navigation from '../navigation/Navigation';
 import {useMediaQuery} from '@/lib/hooks/useMediaQuery';
 import SlidesMobile from './mobile/SlidesMobile';
-import {useState} from 'react';
+import {useState, createContext, useEffect} from 'react';
 import {CardType} from '../../type/card-type';
 import {projects} from '../card/project-card/projects';
+import {imitation} from '../card/imitation-card/imitation';
+import {team} from '../card/team-card/team';
+
+type CurSlideContext = {
+  curSlideTxt: string
+  setCurSlideTxt: React.Dispatch<React.SetStateAction<string>>
+}
+export const CurSlideContext = createContext<CurSlideContext | null>(null);
 
 export default function Slides() {
   const [curSlide, setCurSlide] = useState<Record<string, CardType>>(projects);
+  const [curSlideTxt, setCurSlideTxt] = useState<string>("project");
+
+  useEffect(() => {
+    switch(curSlideTxt) {
+      case "project":
+        setCurSlide(projects);
+        break;
+      case "imitation":
+        setCurSlide(imitation);
+        break;
+      case "team":
+        setCurSlide(team);
+        break;
+    }
+  }, [curSlideTxt]);
+
   const {width, isMobile} = useMediaQuery();
   if(!width) return null;
 
-
   return (
     <div className={styles.slides__wrap}>
-      <Navigation />
+      <CurSlideContext.Provider value={{curSlideTxt, setCurSlideTxt}}>
+        <Navigation />
+      </CurSlideContext.Provider>
       {isMobile && <SlidesMobile curSlide={curSlide} />}
     </div>
   );
