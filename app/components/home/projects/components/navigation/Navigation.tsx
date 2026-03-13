cbe6b6b9e4684f09738a9f26f868a879e292fa5f
Jwiroo <qorudgus77@gmail.com>
2026-03-13 12:28:54 +0900

    chore(projects): create Navigation component file

---
+++ app/components/home/projects/components/navigation/Navigation.tsx
@@ -0,0 +1,12 @@
+import {useMediaQuery} from "@/lib/hooks/useMediaQuery";
+import NavigationMobile from "./mobile/navigationMobile";
+
+export default function Navigation() {
+  const {width, isMobile} = useMediaQuery();
+
+  if(!width) return null;
+
+  if(isMobile) {
+    return <NavigationMobile />
+  }
+}
