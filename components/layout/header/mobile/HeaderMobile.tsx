57323b3e21d1a4bbd7a761050673015d4c560853
Jwiroo <qorudgus77@gmail.com>
2026-03-16 14:48:02 +0900

    style(header): change document scrolling and classes based on state

--- components/layout/header/mobile/HeaderMobile.tsx
+++ components/layout/header/mobile/HeaderMobile.tsx
@@ -3,7 +3,7 @@
 import styles from './header-mobile.module.scss';
 import Hamburger from './components/hamburger/Hamburger';
 import Close from './components/close/Close';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Menu from './components/menu/Menu';
 import clsx from 'clsx';
 
@@ -14,10 +14,22 @@ export default function HeaderMobile({isMobile}: {isMobile: boolean}) {
     setIsMenuOpen(prev => !prev);
   }
 
+  useEffect(() => {
+    const html = document.querySelector("html");
+    if(!(html instanceof HTMLElement)) return;
+    
+    if(isMenuOpen) {
+      html.style.setProperty("overflow-y", "hidden");
+    } else {
+      html.style.removeProperty("overflow-y");
+    }
+  }, [isMenuOpen]);
+
   return (
     <header className={clsx(
       styles.header,
-      !isMobile && styles.hide
+      !isMobile && styles.hide,
+      isMenuOpen && styles["menu-open"]
     )}>
       <button className={`${styles["header__menu-btn"]} cursor-target`} onClick={menuBtnClick}>
         {!isMenuOpen ? <Hamburger /> : <Close />}
