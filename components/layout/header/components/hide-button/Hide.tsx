ce47d7ae6b2fbd17c887072c1e88327da11116fe
Jwiroo <qorudgus77@gmail.com>
2026-03-16 18:55:23 +0900

    feat(header): toggle class based on component state

--- components/layout/header/components/hide-button/Hide.tsx
+++ components/layout/header/components/hide-button/Hide.tsx
@@ -1,7 +1,10 @@
 import styles from './hide.module.scss';
 import ButtonWrapper from '@layout/assets/svg/button-wrapper.svg';
 import {useContext, createContext} from 'react';
+import {PageContext} from '../stats/Stats';
+import {pageList} from '@/lib/constants/constants';
 import Image from 'next/image';
+import clsx from 'clsx';
 
 type UiHideContextType = {
   uiHide: boolean,
@@ -11,13 +14,17 @@ export const UiHideContext = createContext<UiHideContextType | null>(null);
 
 export default function Hide() {
   const uiHideContext = useContext(UiHideContext);
+  const page = useContext(PageContext);
 
   const hideChange = () => {
     uiHideContext?.setUiHide(prev => !prev);
   }
 
   return (
-    <button className={styles.hide__button} onClick={hideChange}>
+    <button className={clsx(
+      styles.hide__button,
+      !(page === pageList.mainMenu) && "cursor-target"
+    )} onClick={hideChange}>
       <Image src={ButtonWrapper} alt="hide button" width={120} height={20} />
       <span>UI {uiHideContext?.uiHide ? "표시" : "숨김"}</span>
     </button>
