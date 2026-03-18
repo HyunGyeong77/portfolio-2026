007643b8c119c482785e7bd54405793222cc0b96
Jwiroo <qorudgus77@gmail.com>
2026-03-18 15:47:19 +0900

    feat(footer): implement first and last page feature

--- components/layout/footer/mobile/components/arrow-key/components/arrow/Arrow.tsx
+++ components/layout/footer/mobile/components/arrow-key/components/arrow/Arrow.tsx
@@ -5,23 +5,34 @@ import {PageContext} from '@/components/layout/header/components/stats/Stats';
 import {pageList} from '@/lib/constants/constants';
 import clsx from 'clsx';
 import {AnimationContext} from '@/app/components/ClientComponent';
+import {useButtonDisabled} from '@/components/layout/footer/hooks/useButtonDisabled';
 
 type Props = {
   isPrev: boolean
 }
 
 export default function Arrow(props: Props) {
-  const {isPrev} = props;
-  const page = useContext(PageContext);
   const animationContext = useContext(AnimationContext);
   if(!animationContext) return;
 
+  const {isPrev} = props;
+  const page = useContext(PageContext);
+  const {isLastPage} = useButtonDisabled();
+  const disabled = !isPrev && isLastPage;
+  const firstPagePrevBtn = (page === pageList.about) && isPrev;
+
   return (
-    <button className={clsx(
-      styles.arrow,
-      (page !== pageList.mainMenu) && "cursor-target",
-      isPrev && styles.prev
-    )} onClick={() => animationContext({isNext: !isPrev})}
+    <button 
+      className={clsx(
+        styles.arrow,
+        ((page !== pageList.mainMenu) && !disabled) && "cursor-target",
+        isPrev && styles.prev,
+        disabled && styles.disabled
+      )} onClick={() => animationContext({
+        ...(firstPagePrevBtn && {pageName: pageList.mainMenu}),
+        isNext: !isPrev
+      })}
+      disabled={disabled}
     >
       <ArrowSvg />
     </button>
